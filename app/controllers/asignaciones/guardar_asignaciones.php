d2a404f02d3ad2d0eb0b44b9ce0d0a3c578bd75b
Dayo <5enriquetoyo@gmail.com>
2025-04-30 10:59:24 -0400

    notas "terminadas"

--- app/controllers/asignaciones/guardar_asignaciones.php
+++ app/controllers/asignaciones/guardar_asignaciones.php
@@ -49,15 +49,54 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $query_insert->bindParam(':docente_id', $docente_id);
                     $query_insert->execute();
 
+                    $id_asignacion = $pdo->lastInsertId();
+
                     // Agregar a la lista de asignadas
                     $sql_materia = "SELECT nombre_materia FROM materias WHERE id_materia = :materia_id";
                     $query_materia = $pdo->prepare($sql_materia);
                     $query_materia->bindParam(':materia_id', $materia_id);
                     $query_materia->execute();
                     $materia_nombre = $query_materia->fetchColumn();
-
+                    
                     $materiasAsignadas[] = $materia_nombre;
+                
+                // Validar estudiantes de la seccion
+                $sql_estudiantes = "
+                    SELECT id_estudiante 
+                    FROM estudiantes 
+                    WHERE grado_id = :grado_id AND estado = '1'";
+                $query_estudiantes = $pdo->prepare($sql_estudiantes);
+                $query_estudiantes->bindParam(':grado_id', $grado_id);
+                $query_estudiantes->execute();
+                $estudiantes = $query_estudiantes->fetchAll(PDO::FETCH_COLUMN, 0);
+                if (!empty($estudiantes)){
+                    // Validar que no tengan notas los estudiantes
+                    foreach ($estudiantes as $id_estudiante) {
+                        $sql_notas = "
+                            SELECT COUNT(*) 
+                            FROM notas 
+                            WHERE estudiante_id = :id_estudiante AND asignacion_id = LAST_INSERT_ID() AND estado = '1'";
+                        $query_notas = $pdo->prepare($sql_notas);
+                        $query_notas->bindParam(':id_estudiante', $id_estudiante); // Usar directamente $id_estudiante
+                        $query_notas->execute();
+                        $notas_existe = $query_notas->fetchColumn();
+                        
+                        if ($notas_existe > 0) {
+                            // Si ya existen notas, no se insertan nuevas
+                            continue;
+                        } else 
+                        {// Si no existen notas, se insertan nuevas
+                        $sql_insert_notas = "INSERT INTO notas (estudiante_id, asignacion_id, nota_1, nota_2, nota_3, estado, fyh_creacion)
+                            VALUES (:id_estudiante, :id_asignacion, '0', '0', '0', '100', NOW())";
+                        $query_insert_notas = $pdo->prepare($sql_insert_notas);
+                        foreach ($estudiantes as $id_estudiante) {
+                            $query_insert_notas->bindParam(':id_asignacion', $id_asignacion);
+                            $query_insert_notas->bindParam(':id_estudiante', $id_estudiante);
+                            $query_insert_notas->execute();}
+                        }
+                    }   
                 }
+            }
             }
         }
 
